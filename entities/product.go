d19cc7e8eddc46a4fdb79a246b3f135bc46820c2
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-05 22:48:54 +0700

    add temp product entity

---
+++ entities/product.go
@@ -0,0 +1,13 @@
+package entities
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Seller_ID   uint   `json:"seller_id" form:"seller_id"`
+	Name        string `json:"name" form:"name"`
+	Description string `json:"description" form:"description"`
+	Category    string `json:"category" form:"category"`
+	Price       uint   `json:"price" form:"price"`
+	Qty         uint   `json:"qty" form:"qty"`
+}
