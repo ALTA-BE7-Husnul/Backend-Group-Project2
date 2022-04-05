297135d15be1a8e3f38f9cb0e70ca56eea8d94f5
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-05 17:50:06 +0700

    initial commit (3)

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	_userHandler "group-project-2/delivery/handler/user"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterPathUser(e *echo.Echo, uh *_userHandler.UserHandler) {
+
+}
