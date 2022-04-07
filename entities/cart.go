225577faa2c80f403050097c462498f0abdead94
satriacening <satriacening@gmail.com>
2022-04-07 13:17:01 +0700

    add entities

--- entities/cart.go
+++ entities/cart.go
@@ -1,6 +1,9 @@
 package entities
 
 type Cart struct {
-	Buyer_ID   uint
-	Product_ID uint
+	Buyer_ID   uint   `json:"buyer_id" from:"buyer_id"`
+	Product_ID uint   `json:"product_id" from:"product_id"`
+	Quantit    uint   `json:"quantity" from:"quantity"`
+	Total      uint   `json:"total" from:"total"`
+	Status     string `json:"status" from:"status"`
 }
--- entities/user.go
+++ entities/user.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" form:"name"`
-	Email    string `gorm:"unique" json:"email" form:"email"`
-	Address  string `json:"address" form:"address"`
-	Password string `json:"password" form:"password"`
+	Name     string    `json:"name" form:"name"`
+	Email    string    `gorm:"unique" json:"email" form:"email"`
+	Address  string    `json:"address" form:"address"`
+	Password string    `json:"password" form:"password"`
+	Product  []Product `gorm:"foreignkey:Seller_ID"`
+	Cart     []Cart    `gorm:"foreignkey:Buyer_ID"`
 }
