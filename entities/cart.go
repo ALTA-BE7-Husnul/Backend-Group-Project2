9e0249ff5b0d380491b6dd1b21c78a27e7b0ebb4
satriacening <satriacening@gmail.com>
2022-04-07 18:58:26 +0700

    feature add to cart

--- entities/cart.go
+++ entities/cart.go
@@ -1,9 +1,12 @@
 package entities
 
+import "gorm.io/gorm"
+
 type Cart struct {
+	gorm.Model
 	Buyer_ID   uint   `json:"buyer_id" from:"buyer_id"`
 	Product_ID uint   `json:"product_id" from:"product_id"`
-	Quantit    uint   `json:"quantity" from:"quantity"`
+	Quantity   uint   `json:"quantity" from:"quantity"`
 	Total      uint   `json:"total" from:"total"`
-	Status     string `json:"status" from:"status"`
+	Status     string `gorm:"default:unpaid" json:"status" from:"status"`
 }
---
+++ usecase/cart/cart.go
@@ -0,0 +1,23 @@
+package cart
+
+import (
+	"fmt"
+	_entities "group-project-2/entities"
+	_cartRepository "group-project-2/repository/cart"
+)
+
+type CartUseCase struct {
+	cartRepository _cartRepository.CartRepositoryInterface
+}
+
+func NewCartUseCase(cartRepo _cartRepository.CartRepositoryInterface) CartUseCaseInterface {
+	return &CartUseCase{
+		cartRepository: cartRepo,
+	}
+}
+
+func (uuc *CartUseCase) PostCart(cart _entities.Cart, idToken int) (_entities.Cart, int, error) {
+	fmt.Println("ini di usecase ", cart)
+	cart, err := uuc.cartRepository.PostCart(cart, idToken)
+	return cart, idToken, err
+}
